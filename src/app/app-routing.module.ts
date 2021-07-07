94054edb3483572fec1f2cb6f9699662532dd5dc
abenezermulugeta31 <abenezermulugeta31@gmail.com>
2021-07-07 09:44:07 +0300

    Add router rule for the newly created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
+import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
+import { CreateActorComponent } from './actors/create-actor/create-actor.component';
+import { IndexMovieTheatersComponent } from './movie-theaters/index-movie-theaters/index-movie-theaters.component';
+import { CreateMovieTheaterComponent } from './movie-theaters/create-movie-theater/create-movie-theater.component';
+import { CreateMovieComponent } from './movies/create-movie/create-movie.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
+
   {path: 'genres', component:IndexGenresComponent},
-  {path: 'genres/create', component:CreateGenreComponent}
+  {path: 'genres/create', component:CreateGenreComponent},
+  
+  {path: 'actors', component:IndexActorsComponent},
+  {path: 'actors/create', component:CreateActorComponent},
+  
+  {path: 'movietheaters', component:IndexMovieTheatersComponent},
+  {path: 'movietheaters/create', component:CreateMovieTheaterComponent},
+
+  {path: 'movies/create', component:CreateMovieComponent},
 ];
 
 @NgModule({
