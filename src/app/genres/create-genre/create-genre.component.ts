739af4a9253ffa7a1f82a7f963a92f9182096ab6
abenezermulugeta31 <abenezermulugeta31@gmail.com>
2021-07-07 09:31:54 +0300

    Javascript routing to redirect user to Genres page after Saving new Genre record

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-genre',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGenreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  saveChanges(){
+    //... Save Changes Implementation
+    this.router.navigate(['/genres']);
   }
 
 }
