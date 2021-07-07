c0ffc89270af55a1aa17935f44963a482cc40e15
abenezermulugeta31 <abenezermulugeta31@gmail.com>
2021-07-07 09:26:24 +0300

    Create route rule and add button to route ro Create Genres page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
+import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'genres', component:IndexGenresComponent}
+  {path: 'genres', component:IndexGenresComponent},
+  {path: 'genres/create', component:CreateGenreComponent}
 ];
 
 @NgModule({
