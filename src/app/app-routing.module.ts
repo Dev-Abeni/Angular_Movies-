e788153118533385919d2c47aeb458f3fb5f219d
abenezermulugeta31 <abenezermulugeta31@gmail.com>
2021-07-07 09:20:18 +0300

    Define routing rule and create a link in the menu for the Genre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent}
+  {path: '', component:HomeComponent},
+  {path: 'genres', component:IndexGenresComponent}
 ];
 
 @NgModule({
